930810f1483c7f116135b160916aefd1b7e0514b
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-11-17 18:02:11 -0500

    shell implimentation finally

---
+++ README.md
@@ -0,0 +1,9 @@
+### create.sh:
+- path  = p6_jc_app_create(org, account_id, cert_subject, cert_bits, cert_exp, saml_provider, saml_provider_email, role_full_path)
+- path dir/cookies.txt = p6_jc_cookie_file_get(dir)
+- str assertion64 = p6_jc_auth(cookie_file, xsrf, auth)
+- str assertion64 = p6_jc_saml_login(auth)
+- str https://console.jumpcloud.com/userconsole/auth = p6_jc_auth_url_get()
+- str https://console.jumpcloud.com/userconsole/xsrf = p6_jc_xsrf_url_get()
+- str xsrf = p6_jc_xsrf_get(cookie_file)
+
--- lib/create.sh
+++ lib/create.sh
@@ -1,21 +1,192 @@
 ######################################################################
 #<
 #
-# Function:
-#	unkown  = p6_jc_app_create(org, account_id, cert_subject, cert_bits, cert_exp, saml_provider, saml_provider_email, role_full_path)
+# Function: p6_jc__debug(msg)
 #
 #  Args:
-#	org -
-#	account_id -
-#	cert_subject -
-#	cert_bits -
-#	cert_exp -
-#	saml_provider -
-#	saml_provider_email -
-#	role_full_path -
+#	msg - 
+#
+#>
+######################################################################
+p6_jc__debug() {
+    local msg="$1"
+
+    p6_debug "p6_jc: $msg"
+}
+
+######################################################################
+#<
+#
+# Function: str assertion64 = p6_jc_saml_login(auth)
+#
+#  Args:
+#	auth - 
 #
 #  Returns:
-#	unkown -
+#	str - assertion64
+#
+#>
+######################################################################
+p6_jc_saml_login() {
+    local auth="$1"
+
+    local dir=$(p6_transient_create "jc_cookie")
+    local cookie_file=$(p6_jc_cookie_file_get "$dir")
+
+    local xsrf=$(p6_jc_xsrf_get "$cookie_file")
+    local assertion64=$(p6_jc_auth "$cookie_file" "$xsrf" "$auth")
+
+    p6_transient_delete "$dir"
+
+    p6_return_str "$assertion64"
+}
+
+######################################################################
+#<
+#
+# Function: str xsrf = p6_jc_xsrf_get(cookie_file)
+#
+#  Args:
+#	cookie_file - 
+#
+#  Returns:
+#	str - xsrf
+#
+#>
+######################################################################
+p6_jc_xsrf_get() {
+    local cookie_file="$1"
+
+    local url=$(p6_jc_xsrf_url_get)
+
+    p6_file_rmf "$cookie_file"
+
+    local output=$(curl -sL -c $cookie_file $url)
+    local xsrf=$(p6_echo "$output" | p6_json_key_2_value "xsrf" "-")
+    p6_jc__debug "xsrf_get(): [xsrf=$xsrf]"
+
+    p6_return_str "$xsrf"
+}
+
+######################################################################
+#<
+#
+# Function: str assertion64 = p6_jc_auth(cookie_file, xsrf, auth)
+#
+#  Args:
+#	cookie_file - 
+#	xsrf - 
+#	auth - 
+#
+#  Returns:
+#	str - assertion64
+#
+#>
+######################################################################
+p6_jc_auth() {
+    local cookie_file="$1"
+    local xsrf="$2"
+    local auth="$3"
+
+    local url=$(p6_jc_auth_url_get)
+
+    # XXX: login->email is intentional mapping p6 to idp
+    local email=$(p6_obj_item_get "$auth" "login")
+    local password=$(p6_obj_item_get "$auth" "password")
+    local account_alias=$(p6_obj_item_get "$auth" "account_alias")
+
+    local json=$(p6_aws_template_process \
+		     "jc/auth.json" \
+		     "EMAIL=$email" \
+		     "PASSWORD=$password" \
+		     "ACCOUNT=aws-${account_alias}")
+
+    local dir=$(p6_transient_create "jc_auth")
+    local file="$dir/file"
+    p6_file_write "$file" "$json"
+
+    curl -L -s \
+	 -o $dir/response.html \
+	 -b $cookie_file -c $cookie_file \
+	 -H "X-Xsrftoken: $xsrf" \
+	 -H "Content-Type: application/json" \
+	 -d "@$file" \
+	 $url
+
+    # XXX: recode
+    local assertion64=$(grep SAMLResponse $dir/response.html |sed -e 's,.*value=",,' -e 's,".*,,' | recode html)
+
+    p6_transient_delete "$dir"
+
+    p6_return_str "$assertion64"
+}
+
+######################################################################
+#<
+#
+# Function: path dir/cookies.txt = p6_jc_cookie_file_get(dir)
+#
+#  Args:
+#	dir - 
+#
+#  Returns:
+#	path - dir/cookies.txt
+#
+#>
+######################################################################
+p6_jc_cookie_file_get() {
+    local dir="$1"
+
+    p6_return_path "$dir/cookies.txt"
+}
+
+######################################################################
+#<
+#
+# Function: str https://console.jumpcloud.com/userconsole/xsrf = p6_jc_xsrf_url_get()
+#
+#  Returns:
+#	str - https://console.jumpcloud.com/userconsole/xsrf
+#
+#>
+######################################################################
+p6_jc_xsrf_url_get() {
+
+    p6_return_str "https://console.jumpcloud.com/userconsole/xsrf"
+}
+
+######################################################################
+#<
+#
+# Function: str https://console.jumpcloud.com/userconsole/auth = p6_jc_auth_url_get()
+#
+#  Returns:
+#	str - https://console.jumpcloud.com/userconsole/auth
+#
+#>
+######################################################################
+p6_jc_auth_url_get() {
+
+    p6_return_str "https://console.jumpcloud.com/userconsole/auth"
+}
+
+######################################################################
+#<
+#
+# Function: path  = p6_jc_app_create(org, account_id, cert_subject, cert_bits, cert_exp, saml_provider, saml_provider_email, role_full_path)
+#
+#  Args:
+#	org - 
+#	account_id - 
+#	cert_subject - 
+#	cert_bits - 
+#	cert_exp - 
+#	saml_provider - 
+#	saml_provider_email - 
+#	role_full_path - 
+#
+#  Returns:
+#	path - 
 #
 #>
 ######################################################################
